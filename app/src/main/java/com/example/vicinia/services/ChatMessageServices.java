3ed99f9374d4805da1260cb4530ba66d4d496680
Mark Rofail <markm.rofail@gmail.com>
2017-12-04 13:10:33 +0200

    feat: document services package

--- app/src/main/java/com/example/vicinia/services/ChatMessageServices.java
+++ app/src/main/java/com/example/vicinia/services/ChatMessageServices.java
@@ -1,15 +1,22 @@
 package com.example.vicinia.services;
 
+import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import com.example.vicinia.MainActivity;
 import com.example.vicinia.activities.SplashActivity;
 import com.example.vicinia.clients.ChatMessageClient;
+import com.example.vicinia.fragments.ChatMessageFragment;
+import com.example.vicinia.fragments.QuickActionFragment;
 import com.example.vicinia.pojos.HttpRequest;
+import com.example.vicinia.pojos.HttpResponse;
 import com.example.vicinia.utilities.UrlUtilities;
 
-import org.json.*;
-import java.net.*;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.net.URL;
 
 import static com.example.vicinia.utilities.UrlUtilities.buildChatUrl;
 import static com.example.vicinia.utilities.UrlUtilities.buildDetailsUrl;
@@ -18,7 +25,13 @@ import static com.example.vicinia.utilities.UrlUtilities.buildWelcomeUrl;
 public class ChatMessageServices {
     private static final String TAG = "VICINITA/ChatServices";
 
-    public static void getWelcome(){
+    /**
+     * called whenever connection to backend/welcome is required
+     *
+     * @called_from: {@link SplashActivity#onCreate(Bundle)}
+     * @calls: {@link ChatMessageClient.ApiGetTask}
+     */
+    public static void getWelcome() {
         URL requestURL = buildWelcomeUrl();
         UrlUtilities.API_METHODS requestMethod = UrlUtilities.API_METHODS.GET_WELCOME;
 
@@ -27,29 +40,43 @@ public class ChatMessageServices {
         new ChatMessageClient.ApiGetTask().execute(httpRequest);
     }
 
-    public static void onWelcomeResponse(JSONObject response){
-        try{
+    /**
+     * called whenever response from backend/welcome is received
+     * returns uuid and welcome message
+     *
+     * @param response json response from backend/welcome
+     * @called_from: {@link ChatMessageClient#onGetResponse(HttpResponse)}
+     * @calls: {@link SplashActivity#onLoadingFinish(String, String)}
+     */
+    public static void onWelcomeResponse(JSONObject response) {
+        try {
             String uuid = response.getString("uuid");
             String message = response.getString("message");
 
-            Log.v(TAG, "UUID: "+uuid);
+            Log.v(TAG, "UUID: " + uuid);
 
             SplashActivity splashActivity = SplashActivity.getInstance();
             splashActivity.onLoadingFinish(uuid, message);
-        }
-        catch (JSONException e){
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public static void sendChatMessage(String message, double latitude, double longitude){
+    /**
+     * called whenever connection to backend/chat is required
+     *
+     * @called_from: {@link ChatMessageFragment#onChatButton(View)}
+     * {@link QuickActionFragment#onQuickActionButton(QuickActionFragment.QUICK_ACTIONS)}
+     * @calls: {@link ChatMessageClient.ApiGetTask}
+     */
+    public static void sendChatMessage(String message, double latitude, double longitude) {
         URL requestURL = buildChatUrl();
         UrlUtilities.API_METHODS requestMethod = UrlUtilities.API_METHODS.POST_CHAT;
         JSONObject requestBody = new JSONObject();
 
-        if(latitude == 0 && longitude == 0)
+        if (latitude == 0 && longitude == 0)
             MainActivity.getInstance().onGpsError();
-        else{
+        else {
             try {
                 requestBody.put("message", message);
                 requestBody.put("latitude", Double.toString(latitude));
@@ -65,23 +92,40 @@ public class ChatMessageServices {
         }
     }
 
-    public static void onChatResponse(JSONObject response){
-        try{
+    /**
+     * called whenever response from backend/chat is received
+     * response message
+     *
+     * @param response json response from backend/chat
+     * @called_from: {@link ChatMessageClient#onPostResponse(HttpResponse)}
+     * @calls: {@link MainActivity#onReceiveMessage(String)}
+     */
+    public static void onChatResponse(JSONObject response) {
+        try {
             MainActivity mainActivity = MainActivity.getInstance();
             String message = response.getString("message");
             mainActivity.onReceiveMessage(message);
 
-            Log.v(TAG, "Message: "+message);
-        }
-        catch (JSONException e){
+            Log.v(TAG, "Message: " + message);
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
 
-    public static void getDetails(String placeID, double latitude, double longitude){
-        if(latitude == 0 && longitude == 0)
+    /**
+     * called whenever connection to backend/placeDetails is required
+     *
+     * @param placeID   requested place ID
+     * @param latitude  current latitude
+     * @param longitude current longitude
+     * @called_from: {@link ChatMessageFragment#onChatButton(View)}
+     * {@link QuickActionFragment#onQuickActionButton(QuickActionFragment.QUICK_ACTIONS)}
+     * @calls: {@link ChatMessageClient.ApiGetTask}
+     */
+    public static void getDetails(String placeID, double latitude, double longitude) {
+        if (latitude == 0 && longitude == 0)
             MainActivity.getInstance().onGpsError();
-        else{
+        else {
             URL requestURL = buildDetailsUrl(placeID, latitude, longitude);
             UrlUtilities.API_METHODS requestMethod = UrlUtilities.API_METHODS.GET_DETAILS;
 
@@ -91,8 +135,16 @@ public class ChatMessageServices {
         }
     }
 
-    public static void onDetailsResponse(JSONObject response){
-        try{
+    /**
+     * called whenever response from backend/placeDetails is received
+     * response message
+     *
+     * @param response json response from backend/placeDetails
+     * @called_from: {@link ChatMessageClient#onGetResponse(HttpResponse)}
+     * @calls: {@link MainActivity#onReceiveMessage(String)}
+     */
+    public static void onDetailsResponse(JSONObject response) {
+        try {
             MainActivity mainActivity = MainActivity.getInstance();
 
             String name = response.getString("name");
@@ -105,19 +157,17 @@ public class ChatMessageServices {
 
             String message = "";
             message += "<b>Name: </b>" + name + "<br>";
-            message += "<b>Distance: </b>" +distance + "<br>";
-            message += "<b>Rating: </b>" +rating + "<br>";
-            message += "<b>Type: </b>" +type + "<br>";
-            message += "<b>Address: </b>" +address + "<br>";
-            message += "<b>Mobile Number: </b>" +mobileNumber + "<br/><br/>";
+            message += "<b>Distance: </b>" + distance + "<br>";
+            message += "<b>Rating: </b>" + rating + "<br>";
+            message += "<b>Type: </b>" + type + "<br>";
+            message += "<b>Address: </b>" + address + "<br>";
+            message += "<b>Mobile Number: </b>" + mobileNumber + "<br/><br/>";
             message += "<b><font color=\"#1C78C6\"><a href=\"" + link + "\">Open in Google Maps</a></font></b>";
 
             mainActivity.onReceiveMessage(message);
-            Log.v(TAG, "Message: "+message);
-        }
-        catch (JSONException e){
+            Log.v(TAG, "Message: " + message);
+        } catch (JSONException e) {
             e.printStackTrace();
         }
     }
-
 }
--- app/src/main/java/com/example/vicinia/services/GpsServices.java
+++ app/src/main/java/com/example/vicinia/services/GpsServices.java
@@ -4,26 +4,42 @@ import android.content.Context;
 import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
+import com.example.vicinia.MainActivity;
+import com.example.vicinia.fragments.ChatMessageFragment;
+import com.example.vicinia.fragments.QuickActionFragment;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 
+/**
+ * GpsServices: the service responsible to obtaining the device's location
+ */
 public class GpsServices implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener, LocationListener {
-    private static final String TAG = "GOOGLE API";
-    private static final int LONGEST_INTERVAL = 60*1000;
-    private static final int FASTEST_INTERVAL = 10*1000;
+    private static final String TAG = "VICINITA/GpsServices";
+
+    //request location every 1 minute
+    private static final int LONGEST_INTERVAL = 60 * 1000;
+    //obtain location if any other application requests the location in 10s
+    private static final int FASTEST_INTERVAL = 10 * 1000;
 
     private Context mContext;
-
     private GoogleApiClient mGoogleApiClient;
-    private Location mLastLocation;
 
+    private Location mLastLocation;
     private double latitude;
     private double longitude;
 
+    /**
+     * the GpsServices constructor. builds the GoogleApiClient
+     *
+     * @param context is a reference to the calling activity, used when creating the dialog
+     * @called_from: {@link MainActivity#onCreate(Bundle)}
+     * @calls: {@link GoogleApiClient.Builder}
+     */
     public GpsServices(Context context) {
         mContext = context;
 
@@ -34,38 +50,47 @@ public class GpsServices implements GoogleApiClient.ConnectionCallbacks, GoogleA
                 .build();
     }
 
+    /**
+     * connects the client created in {@link #GpsServices(Context)}
+     *
+     * @called_from: {@link MainActivity#onStart()}
+     * @calls: none
+     */
     public void onStart() {
         mGoogleApiClient.connect();
     }
 
+    /**
+     * disconnects the client if connected
+     *
+     * @called_from: {@link MainActivity#onStop()}
+     * @calls: none
+     */
     public void onStop() {
         if (mGoogleApiClient.isConnected()) {
             mGoogleApiClient.disconnect();
         }
     }
 
+    /**
+     * callback function when API is connected
+     *
+     * @param connectionHint bundle/map containing information about the connection
+     */
     @Override
     public void onConnected(Bundle connectionHint) {
         LocationRequest locationRequest = createLocationRequest();
-        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient,locationRequest, this);
+        LocationServices.FusedLocationApi.requestLocationUpdates(mGoogleApiClient, locationRequest, this);
         getLastLocation();
     }
 
-    @Override
-    public void onConnectionFailed(ConnectionResult result) {
-        // Refer to the javadoc for ConnectionResult to see what error codes might be returned in
-        // onConnectionFailed.
-        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
-    }
-
-    @Override
-    public void onConnectionSuspended(int cause) {
-        // The connection to Google Play services was lost for some reason. We call connect() to
-        // attempt to re-establish the connection.
-        Log.i(TAG, "Connection suspended");
-        mGoogleApiClient.connect();
-    }
-
+    /**
+     * sets intervals for the location request
+     *
+     * @return location request object
+     * @called_from: {@link #onConnected(Bundle)} (Location)}
+     * @calls: none
+     */
     private LocationRequest createLocationRequest() {
         LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setInterval(LONGEST_INTERVAL);
@@ -74,6 +99,45 @@ public class GpsServices implements GoogleApiClient.ConnectionCallbacks, GoogleA
         return mLocationRequest;
     }
 
+    /**
+     * callback function when API can't connect
+     *
+     * @param result object containing information about the connection
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    /**
+     * callback function when API is suspended
+     *
+     * @param cause error code
+     */
+    @Override
+    public void onConnectionSuspended(int cause) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
+    }
+
+    /**
+     * callback function whenever the location changes
+     *
+     * @param location updated location
+     */
+    @Override
+    public void onLocationChanged(Location location) {
+        mLastLocation = location;
+        getLastLocation();
+    }
+
+    /**
+     * updates cached location
+     *
+     * @return new location
+     * @called_from: {@link #onLocationChanged(Location)}
+     * @calls: none
+     */
     public Location getLastLocation() {
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
 
@@ -83,8 +147,7 @@ public class GpsServices implements GoogleApiClient.ConnectionCallbacks, GoogleA
             longitude = mLastLocation.getLongitude();
             latitude = mLastLocation.getLatitude();
 
-            Log.v(TAG, "LOCATION "+latitude +", "+longitude);
-
+            Log.v(TAG, "LOCATION " + latitude + ", " + longitude);
         } else {
             Log.v(TAG, "No location detected, Make sure that location is enabled in the device");
         }
@@ -92,16 +155,28 @@ public class GpsServices implements GoogleApiClient.ConnectionCallbacks, GoogleA
         return this.mLastLocation;
     }
 
-    @Override
-    public void onLocationChanged(Location location) {
-        mLastLocation = location;
-        getLastLocation();
-    }
-
+    /**
+     * getter for longitude
+     *
+     * @return cached longitude
+     * @called_from: {@link QuickActionFragment#onQuickActionButton(QuickActionFragment.QUICK_ACTIONS)}
+     * {@link MainActivity#onGetDetailsButton(String)}
+     * {@link ChatMessageFragment#onChatButton(View)}
+     * @calls: none
+     */
     public double getLongitude() {
         return longitude;
     }
 
+    /**
+     * getter for latitude
+     *
+     * @return cached latitude
+     * @called_from: {@link QuickActionFragment#onQuickActionButton(QuickActionFragment.QUICK_ACTIONS)}
+     * {@link MainActivity#onGetDetailsButton(String)}
+     * {@link ChatMessageFragment#onChatButton(View)}
+     * @calls: none
+     */
     public double getLatitude() {
         return latitude;
     }
